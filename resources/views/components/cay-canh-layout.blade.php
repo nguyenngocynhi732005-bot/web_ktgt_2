cc5e6c80d00979d1023046fb3bdc1d3ef89b763a
Ý Nhi <030239230162@st.buh.edu.vn>
2026-04-15 14:01:27 +0700

    đăng nhập đăng kí

--- resources/views/components/cay-canh-layout.blade.php
+++ resources/views/components/cay-canh-layout.blade.php
@@ -154,7 +154,7 @@
                                 {{ Auth::user()->name }}
                                 </button>
                                 <div class="dropdown-menu">
-                                <a class="dropdown-item" href="">Quản lý</a>
+                                <a class="dropdown-item" href="{{ route('dashboard') }}">Quản lý</a>
                                 <form method="POST" action="{{ route('logout') }}">
                                     @csrf
                                     <a class="dropdown-item" onclick="event.preventDefault();
